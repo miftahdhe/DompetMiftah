6b4c36ba5c3e8ce9c2e2a5fcedd0475f64d9efe7
miftahdhe <miftahdhe@gmail.com>
2026-06-29 20:32:30 +0400

    Add wallet storage service

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/wallet_model.dart';
+
+class StorageService {
+  static const String walletKey = "wallets";
+
+  static Future<void> saveWallets(List<WalletModel> wallets) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final data = wallets.map((e) => e.toJson()).toList();
+
+    await prefs.setString(walletKey, jsonEncode(data));
+  }
+
+  static Future<List<WalletModel>> loadWallets() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final json = prefs.getString(walletKey);
+
+    if (json == null) return [];
+
+    final List list = jsonDecode(json);
+
+    return list.map((e) => WalletModel.fromJson(e)).toList();
+  }
+}
